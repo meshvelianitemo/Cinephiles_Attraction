53db89a7639558793652d8152029f7f8fee46e56
unknown <temomeshveliani777@gmail.com>
2025-10-08 01:23:53 +0400

    aha axali comiti

--- Login.Designer.cs
+++ Login.Designer.cs
@@ -31,6 +31,13 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.Username_txtb = new System.Windows.Forms.TextBox();
+            this.Password_txtb = new System.Windows.Forms.TextBox();
+            this.Error_label = new System.Windows.Forms.Label();
+            this.Login_btn = new System.Windows.Forms.Button();
+            this.Register_btn = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -56,7 +63,7 @@
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(100, 23);
             this.label2.TabIndex = 1;
-            this.label2.Text = "authentification";
+            this.label2.Text = "Username : ";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label3
@@ -69,20 +76,99 @@
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(100, 23);
             this.label3.TabIndex = 2;
-            this.label3.Text = "authentification";
+            this.label3.Text = "Password : ";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Username_txtb
+            // 
+            this.Username_txtb.Location = new System.Drawing.Point(174, 81);
+            this.Username_txtb.Name = "Username_txtb";
+            this.Username_txtb.Size = new System.Drawing.Size(184, 20);
+            this.Username_txtb.TabIndex = 3;
+            // 
+            // Password_txtb
+            // 
+            this.Password_txtb.Location = new System.Drawing.Point(174, 127);
+            this.Password_txtb.Name = "Password_txtb";
+            this.Password_txtb.Size = new System.Drawing.Size(184, 20);
+            this.Password_txtb.TabIndex = 4;
+            // 
+            // Error_label
+            // 
+            this.Error_label.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Error_label.BackColor = System.Drawing.Color.White;
+            this.Error_label.ForeColor = System.Drawing.Color.Red;
+            this.Error_label.Location = new System.Drawing.Point(51, 165);
+            this.Error_label.Name = "Error_label";
+            this.Error_label.Size = new System.Drawing.Size(406, 23);
+            this.Error_label.TabIndex = 5;
+            this.Error_label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Login_btn
+            // 
+            this.Login_btn.Location = new System.Drawing.Point(34, 211);
+            this.Login_btn.Name = "Login_btn";
+            this.Login_btn.Size = new System.Drawing.Size(127, 38);
+            this.Login_btn.TabIndex = 6;
+            this.Login_btn.Text = "Login";
+            this.Login_btn.UseVisualStyleBackColor = true;
+            // 
+            // Register_btn
+            // 
+            this.Register_btn.Location = new System.Drawing.Point(332, 210);
+            this.Register_btn.Name = "Register_btn";
+            this.Register_btn.Size = new System.Drawing.Size(125, 39);
+            this.Register_btn.TabIndex = 7;
+            this.Register_btn.Text = "Register";
+            this.Register_btn.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label4.Location = new System.Drawing.Point(189, 211);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(120, 23);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "dont have Account? ";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label5.Location = new System.Drawing.Point(189, 234);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(120, 23);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Click here -->";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(492, 275);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Register_btn);
+            this.Controls.Add(this.Login_btn);
+            this.Controls.Add(this.Error_label);
+            this.Controls.Add(this.Password_txtb);
+            this.Controls.Add(this.Username_txtb);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Login";
             this.Text = "Login";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -91,5 +177,12 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Username_txtb;
+        private System.Windows.Forms.TextBox Password_txtb;
+        private System.Windows.Forms.Label Error_label;
+        private System.Windows.Forms.Button Login_btn;
+        private System.Windows.Forms.Button Register_btn;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
